0f2b3a7d44d9a7ef056a5d70626439696ddc9c26
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-04-17 23:10:22 +0100

    Updated vulnerable deps

--- app/src/index.js
+++ app/src/index.js
@@ -2,3 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
