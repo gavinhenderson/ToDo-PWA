6cf6f2c6b12c1a2409503588bd4c1097a2f38bf8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-04-22 20:42:16 +0100

    Can now install from a button press@

--- app/src/app.js
+++ app/src/app.js
@@ -8,7 +8,25 @@ class App extends React.Component {
 
     this.state = {
       todos: [],
+      installApp: null,
     };
+  }
+
+  componentDidMount() {
+    window.addEventListener(
+      'beforeinstallprompt',
+      this.handleInstall.bind(this),
+    );
+  }
+
+  handleInstall(event) {
+    event.preventDefault();
+
+    const promptFunc = (e) => {
+      event.prompt();
+    };
+
+    this.setState({ installApp: promptFunc });
   }
 
   addTodo = () => {
@@ -19,14 +37,16 @@ class App extends React.Component {
   };
 
   render() {
+    const { installApp, todos } = this.state;
+
     return (
       <div>
-        <button>Install</button>
+        {installApp && <button onClick={installApp}>Install</button>}
         <p>Add a new todo:</p>
         <input ref={this.newTodo} />
         <button onClick={this.addTodo}>Add</button>
         <ul>
-          {this.state.todos.map((todo, index) => (
+          {todos.map((todo, index) => (
             <li key={`todo-${index}`}>{todo}</li>
           ))}
         </ul>
