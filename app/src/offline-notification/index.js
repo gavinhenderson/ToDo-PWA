e687293d64cb68346170687e17263e58e5082c8f
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-04-25 00:06:26 +0100

    Aded offline notification to the bottom of the app

--- app/src/offline-notification/index.js
+++ app/src/offline-notification/index.js
@@ -5,21 +5,42 @@ class OfflineNotification extends React.Component {
   constructor() {
     super();
 
-    this.state = { open: false };
+    this.state = { queue: [], open: false, message: '' };
+  }
+
+  dispatchEvent() {
+    const latest = this.state.queue[this.state.queue.length - 1];
+
+    if (latest) {
+      this.setState({ message: latest.message, open: latest.open });
+    }
+  }
+
+  addToQueue(message) {
+    this.setState({ queue: [{ message, open: true }, ...this.state.queue] });
+
+    if (this.state.queue.length === 1) this.dispatchEvent();
   }
 
   componentDidMount() {
     window.addEventListener('online', () => {
-      this.setState({ message: 'Navigator has come back online', open: true });
+      this.addToQueue('Navigator has come back online');
     });
 
     window.addEventListener('offline', () => {
-      this.setState({ message: 'Navigator has gone offline', open: true });
+      this.addToQueue('Navigator has gone offline');
     });
   }
 
   handleClose = () => {
-    this.setState({ open: false });
+    this.setState({
+      open: false,
+      queue: this.state.queue.slice(0, this.state.queue.length - 1),
+    });
+
+    setTimeout(() => {
+      this.dispatchEvent();
+    }, 500);
   };
 
   render() {
