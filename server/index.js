e766402ec882bd702a76bf6f5b43d3e04696c9f6
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-04-25 16:32:43 +0100

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const PORT = process.env.PORT || 3000;
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!!'));
 
